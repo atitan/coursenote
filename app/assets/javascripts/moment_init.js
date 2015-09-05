8a8333ad256f4893bb53272edb8fefd1d944c4c7
mitour <love2082020@gmail.com>
2015-09-05 12:56:22 +0000

    Remove console.log form moment_init.js

--- app/assets/javascripts/moment_init.js
+++ app/assets/javascripts/moment_init.js
@@ -1,6 +1,5 @@
 var moment = function() {
 	$('.date').each(function() {
-		console.log('test');
 		var date = $(this).data('date');
 		var date_formatted = moment(date).startOf('minute').fromNow();
 		$(this).text(date_formatted);
