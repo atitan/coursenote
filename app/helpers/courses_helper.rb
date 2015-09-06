76573b3525d5e23bfcab5a62c69a0437ca78b1eb
ATI <commit@atifans.net>
2015-09-06 23:56:04 +0800

    add time_overlap check on entries

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,7 @@
 module CoursesHelper
+  def time_overlap?(entry, current_user)
+    time_filter = current_user.time_filter
+    return '' if time_filter.empty? or time_filter.merge(entry.timetable) == time_filter
+    'background-color: #FFCCCC'
+  end
 end
