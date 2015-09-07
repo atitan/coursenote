7f53e766653b8c7648532aba18bc3847926c1bb6
ATI <commit@atifans.net>
2015-09-07 16:00:12 +0800

    fix current_user is nil when checking timetable

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,5 +1,7 @@
 module CoursesHelper
-  def time_overlap?(timetable, time_filter)
+  def time_overlap?(timetable, current_user)
+    return '' if current_user.nil?
+    time_filter = current_user.time_filter
     nonoverlap = time_filter.empty? || timetable.keys.all?{|key| time_filter.has_key?(key) && (timetable[key] - time_filter[key]).empty?}
     nonoverlap ? '' : 'background-color: #FFCCCC'
   end
