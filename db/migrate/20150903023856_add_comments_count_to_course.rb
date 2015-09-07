ac38065d0fc7fbd9dc7550c6ad9a467a222f7cab
Yusheng Li <tamamali@icloud.com>
2015-09-07 06:26:21 +0000

    Remove counter_cache in Comment and delete unnecessary column

--- db/migrate/20150903023856_add_comments_count_to_course.rb
+++
@@ -1,9 +0,0 @@
-class AddCommentsCountToCourse < ActiveRecord::Migration
-  def change
-    add_column :courses, :comments_count, :integer, default: 0
-
-    Course.pluck(:id).each do |i|
-      Course.reset_counters(i, :comments)
-    end
-  end
-end
