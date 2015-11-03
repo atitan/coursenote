b7a61e8c4901c35c2bd582d38f998afe9a671474
ATI <commit@atifans.net>
2015-11-03 22:55:13 +0800

    preselect searchform checkbox

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -5,4 +5,13 @@ module CoursesHelper
     nonoverlap = time_filter.empty? || timetable.keys.all?{|key| time_filter.has_key?(key) && (timetable[key] - time_filter[key]).empty?}
     nonoverlap ? '' : 'background-color: #FFCCCC'
   end
+
+  def category_button(name)
+    active = params[:by_category].include?(name) unless params[:by_category].nil?
+    if active
+      '<label class="btn btn-warning mg-b-10 active"><input type="checkbox" name="by_category[]" value="'+ name +'" checked>'+ name +'</label>'
+    else
+      '<label class="btn btn-warning mg-b-10"><input type="checkbox" name="by_category[]" value="'+ name +'">'+ name +'</label>'
+    end
+  end
 end
