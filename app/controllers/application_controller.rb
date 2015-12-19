2edb1758d75dab12692ead757ba01a09320d6b6b
ATI <commit@atifans.net>
2015-12-19 18:07:07 +0800

    temporarily handle exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,11 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   after_filter :set_csrf_cookie_for_ng
 
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::ParameterMissing, with: :param_missing
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_auth_token
+  rescue_from ActionController::UnknownFormat, with: :unknown_format
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
@@ -25,21 +28,23 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def record_not_unique
+    render text: "Record Not Unique", status: 406
+  end
+
+  def unknown_format
+    render text: "Unknown Format", status: 406
+  end
+
+  def invalid_auth_token
+    render text: "Invalid Authenticity Token", status: 422
+  end
+
   def record_not_found
-    respond_to do |format|
-      format.html
-      format.json {
-        render json: { error: 'NotFound' }, status: :not_found
-      }
-    end
+    render text: "Not Found", status: 404
   end
 
   def param_missing
-    respond_to do |format|
-      format.html
-      format.json {
-        render json: { error: 'ParameterMissing' }, status: :bad_request
-      }
-    end
+    render text: "Parameter Missing", status: 422
   end
 end
