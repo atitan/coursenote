da1e2090bd4fbc414b7186489392edee90a1e8dd
ATI <commit@atifans.net>
2016-01-07 20:18:51 +0800

    fix error

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,7 +3,7 @@ class SitemapController < ApplicationController
 
   def course
     @most_recent = Course.select(:updated_at).order(updated_at: :desc).limit(1).first
-    if stale?(etag: @most_recent, last_modified: @most_recent.updated_at.utc.rfc822)
+    if stale?(etag: @most_recent, last_modified: @most_recent.updated_at)
       respond_to do |format|
         format.xml { @courses = Course.select(:id, :updated_at) }
       end
