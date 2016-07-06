0d2569f6b7828e515d4c692b9064eabe66feffaf
ATI <commit@atifans.net>
2016-07-06 18:39:44 +0800

    replace before_filter with before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :flash_write_back, if: :user_signed_in?
-  before_filter :set_paper_trail_whodunnit
+  before_action :flash_write_back, if: :user_signed_in?
+  before_action :set_paper_trail_whodunnit
 
   protected
 
