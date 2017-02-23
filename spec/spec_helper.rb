0fb06861a3b1bbc2414c54edba12b3ab19853c0a
ATI <commit@atifans.net>
2017-02-24 04:36:30 +0800

    upgrade to ruby 2.4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 require 'simplecov'
 SimpleCov.start
 
