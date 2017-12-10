6c41697e0b3a4d6690cdfa6b828ec99827a3671a
ATI <commit@atifans.net>
2017-12-10 22:38:40 +0800

    upgrade dependency to fix redis-store CVE

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,5 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-FactoryGirl.define do
+# Read about factories at https://github.com/thoughtbot/factory_bot
+FactoryBot.define do
   factory :course do
     title '宗教哲學'
     category '天'
